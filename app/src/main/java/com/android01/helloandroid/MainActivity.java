45d39f565a2595081fa7d8c589be62f54fbee0c8
yahyahudan19 <yahyahudan19@gmail.com>
2020-02-07 14:39:15 +0700

    Tugas 01 Pemograman Mobile

---
+++ app/src/main/java/com/android01/helloandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android01.helloandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
